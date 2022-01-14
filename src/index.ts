e4910db5df2fa5c3cb1d746d9af413741b1f89a5
Katie Bell <katie@katharos.id.au>
2022-01-15 10:34:03 +1100

    Add demo code for python with and without atomics.

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,21 @@ import WasmTerminalConfig from "./wasm-terminal-config";
 import 'xterm/css/xterm.css'
 import { StandardIO, WorkerManager } from "./worker-manager";
 
+import "./styles.css"
+
 const consoleCode = `
 import code
 code.interact()
 `
 
+const runButton = document.getElementById('run')
+const replButton = document.getElementById('repl')
+const clearButton = document.getElementById('clear')
+
 window.onload = () => {
     const config = new WasmTerminalConfig({fetchCommand: null, processWorkerUrl: ''})
     const terminal = new WasmTerminal(config)
-    terminal.open(document.getElementById('app-root'))
+    terminal.open(document.getElementById('terminal'))
 
     const stdio: StandardIO = {
         stdout: (s) => { terminal.print(s) },
@@ -22,6 +28,21 @@ window.onload = () => {
         }
     }
 
+    runButton.addEventListener('click', (e) => {
+        const code = (document.getElementById('code') as HTMLTextAreaElement).value
+        pythonWorkerManager.runCode(code)
+    })
+    runButton.removeAttribute('disabled')
+
+    replButton.addEventListener('click', (e) => {
+        pythonWorkerManager.runCode(consoleCode)
+    })
+    replButton.removeAttribute('disabled')
+
+    clearButton.addEventListener('click', (e) => {
+        terminal.clear()
+    })
+    clearButton.removeAttribute('disabled')
+
     const pythonWorkerManager = new WorkerManager('/python-webworker.js', stdio)
-    pythonWorkerManager.runCode(consoleCode)
 }
